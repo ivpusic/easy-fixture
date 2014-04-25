085335c37fe64b8f9980147015fe55db824971b1
ivpusic <pusic007@gmail.com>
2014-04-25 03:07:48 +0200

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 'use strict';
 
+var manager = null;
+
 module.exports = {
-  MongoFixture: require('./lib/mongo_fixture'),
-  Base: require('./lib/base')
+  Base: require('./lib/base'),
+
+  use: function (fixtureManager) {
+    if (!fixtureManager || !fixtureManager._save || !fixtureManager._load) {
+      throw new Error('Plase provide instance of Base fixture class, with implemented interface.');
+    }
+
+    manager = fixtureManager;
+  },
+
+  save: function () {
+    return manager._save();
+  },
+
+  load: function () {
+    return manager._load();
+  }
 };
